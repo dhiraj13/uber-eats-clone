e7c343390b7323c713c37226e378a0f6b4765690
Dhiraj Khapangi <dkhapangi13@outlook.com>
2022-10-24 21:43:50 -0700

    header tabs

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Universal React with Expo</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
