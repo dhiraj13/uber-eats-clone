3f027b8468a32d79eb5ee55357612eca2315945d
Dhiraj Khapangi <dkhapangi13@outlook.com>
2022-10-26 17:41:58 +0545

    make about page data dynamic

--- components/restaurantDetail/About.js
+++ components/restaurantDetail/About.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { View, Text, Image } from 'react-native'
 
-const image =
-  'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'
+const yelpRestaurantInfo = {
+  name: 'Farmhouse Kitchen Thai Cuisine',
+  image:
+    'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudCUyMGludGVyaW9yfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+  price: '$$',
+  reviews: '1500',
+  rating: 4.5,
+  categories: [{ title: 'Thai' }, { title: 'Comfort Food' }],
+}
 
-const title = 'Farmhouse Kitchen Thai Cuisine'
-const description = 'Thai • Comfort Food • $$ • 🎫 • 4 ⭐ (2913+)'
+const { name, image, price, reviews, rating, categories } = yelpRestaurantInfo
+
+const formattedCategories = categories.map((cat) => cat.title).join(' • ')
+
+const description = `${formattedCategories} ${
+  price ? ' • ' + price : ''
+} • 🎫 • ${rating} ⭐ (${reviews}+)`
 
 export default function About() {
   return (
     <View>
       <RestaurantImage image={image} />
-      <RestaurantTitle text={title} />
+      <RestaurantName name={name} />
       <RestaurantDescription description={description} />
     </View>
   )
@@ -21,7 +33,7 @@ const RestaurantImage = ({ image }) => (
   <Image source={{ uri: image }} style={{ width: '100%', height: 180 }} />
 )
 
-const RestaurantTitle = ({ text }) => (
+const RestaurantName = ({ name }) => (
   <Text
     style={{
       fontSize: 29,
@@ -30,7 +42,7 @@ const RestaurantTitle = ({ text }) => (
       marginHorizontal: 15,
     }}
   >
-    {text}
+    {name}
   </Text>
 )
 
